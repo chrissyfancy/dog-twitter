04ea636a8b9a8209a70de5c3c483f01fc033852a
Amanda Beiner <aebeiner@gmail.com>
2017-03-15 14:05:58 -0400

    initial commit

---
+++ src/constants/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    id_str: '725090047557558272',
+    text: "This doesn't look like Kansas",
+    user: {
+       name: 'Todo',
+       profile_image_url: 'http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg'
+    }
+  },
+  {
+    id_str: '725028750535102466',
+    text: "Must be all squeaky clean for the New World",
+    user: {
+       name: 'Percy',
+       profile_image_url: "https://blahblahblonde1.files.wordpress.com/2013/01/percy.jpg"
+    }
+  },
+  {
+    id_str: '1461003902000',
+    text: "Many Halloweens ago, I was just a tiny red puppy.",
+    user: {
+       name: 'Clifford the Big Red Dog',
+       profile_image_url: 'http://1.bp.blogspot.com/-fuLcmzFOP7Y/VRw7NET0OoI/AAAAAAAAKWM/WzUFezVlKp8/s1600/Clifford%2Bthe%2BBig%2BRed%2BDog.jpg'
+    }
+  },
+  {
+    id_str: '702194866168528896',
+    text: 'BALL! BALL! BALL!',
+    user: {
+       name: 'Air Bud',
+       profile_image_url: "https://s-media-cache-ak0.pinimg.com/736x/de/b0/de/deb0de35af4623ab167c2cdcb0b1e254.jpg"
+    }
+  },
+  {
+    id_str: '702258314550202369',
+    text: "I do not like the cone of shame",
+    user: {
+       name: 'Dug',
+       profile_image_url: "http://vignette2.wikia.nocookie.net/pixar/images/3/35/Dug-up.jpg/revision/latest?cb=20090602035123"
+    }
+  },
+  {
+    id_str: '7022583145502837499',
+    text: "buh da-daaaaa, buh da-daaaaa...",
+    user: {
+       name: 'Butkus',
+       profile_image_url: "https://s-media-cache-ak0.pinimg.com/236x/81/bf/25/81bf25311796e043c9e44058a3a850e7.jpg"
+    }
+  }
+];
+
+export default data;
